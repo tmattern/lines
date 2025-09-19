40ad2b356df0c12fabe585e2854731f92d4664b3
Thibaut Mattern <tmattern@adlere.fr>
2025-09-19 17:12:47 +0200

    optim zone reduite

--- lib/clear_screen.asm
+++ lib/clear_screen.asm
@@ -3,37 +3,8 @@
 ; Utilise : X, A uniquement
 ;==============================================================================
 
-ClearScreen_v1:
-    pshs  a,x,u
-    ldx   #$4000         ; Début VRAM
-    ldu   #0             ; U = 0 (valeur à écrire)
-    lda   #200           ; A = 200 lignes
-CS_Loop:
-    stu   0,x
-    stu   2,x
-    stu   4,x
-    stu   6,x
-    stu   8,x
-    stu   10,x
-    stu   12,x
-    stu   14,x
-    stu   16,x
-    stu   18,x
-    stu   20,x
-    stu   22,x
-    stu   24,x
-    stu   26,x
-    stu   28,x
-    stu   30,x
-    stu   32,x
-    stu   34,x
-    stu   36,x
-    stu   38,x
-    deca
-    bne   CS_Loop
-    puls  a,x,u,pc
 
-ClearScreen_v2:
+ClearScreen:
     pshs  a,b,x,y,u,cc,dp
     clrd
     ldx   #0
@@ -42,7 +13,7 @@ ClearScreen_v2:
     andcc #0
     ldu   #$5F40
     
-CS_Loop_v2:
+CS_Loop:
     ; ligne 1
     pshu  a,b,x,y,cc,dp
     pshu  a,b,x,y,cc,dp
@@ -79,7 +50,7 @@ CS_Loop_v2:
     pshu  a,b,x,y,cc,dp
 
     cmpu  #$4000
-    bne   CS_Loop_v2
+    bne   CS_Loop
     puls  a,b,x,y,u,cc,dp,pc
 
 
@@ -87,29 +58,51 @@ CS_Loop_v2:
 ; Effacement d'une zone centrée de 192x120 pixels (24 octets x 120 lignes)
 ; Centre horizontal = 8 octets (64px) depuis la gauche, vertical = 40 lignes
 ; Utilise : X, Y, B, A, TMP (RAM)
+; Marge haut    $4000   $49E7
+; Zone centrée  $4A08   $5EDF
+; Marge bas     $5F00   $5FA7
 ;==============================================================================
 
 ClearZone_Centered:
-    ldy   #120           ; 120 lignes à effacer
-CZ_Ligne:
-    ldb   #40            ; 40 octets/ligne
-    sty   TMP            ; Sauvegarde Y (compteur lignes)
-    ldy   TMP            ; Y = ligne courante (0..119)
-    mul                  ; D = Y * 40
-    addd  #$4008         ; +8 pour centrer (8 octets = 64px)
-    tfr   d,x            ; X = début de la zone à effacer
+    pshs  a,b,x,y,u,cc,dp
+    clrd
+    ldx   #0
+    ldy   #0
+    tfr   a,dp
+    andcc #0
+    ldu   #$5EE0
+    
+CS_Loop_Centered:
+    ; ligne 1
+    pshu  a,b,x,y,cc,dp
+    pshu  a,b,x,y,cc,dp
+    pshu  a,b,x,y,cc,dp
+    leau  -16,u
 
-    ldb   #24            ; 24 octets = 192px
-    clra                 ; A = 0 à écrire
-CZ_Zone:
-    sta   ,x+
-    decb
-    bne   CZ_Zone
+    ; ligne 2
+    pshu  a,b,x,y,cc,dp
+    pshu  a,b,x,y,cc,dp
+    pshu  a,b,x,y,cc,dp
+    leau  -16,u
 
-    ldy   TMP            ; Restaure Y
-    leay  -1,y
-    cmpy  #0
-    bne   CZ_Ligne
-    rts
+    ; ligne 3
+    pshu  a,b,x,y,cc,dp
+    pshu  a,b,x,y,cc,dp
+    pshu  a,b,x,y,cc,dp
+    leau  -16,u
 
-TMP   rmb 2              ; Variable temporaire pour la ligne (2 octets)
+    ; ligne 4
+    pshu  a,b,x,y,cc,dp
+    pshu  a,b,x,y,cc,dp
+    pshu  a,b,x,y,cc,dp
+    leau  -16,u
+
+    ; ligne 5
+    pshu  a,b,x,y,cc,dp
+    pshu  a,b,x,y,cc,dp
+    pshu  a,b,x,y,cc,dp
+
+    cmpu  #$4A08
+    leau  -16,u
+    bne   CS_Loop_Centered
+    puls  a,b,x,y,u,cc,dp,pc
