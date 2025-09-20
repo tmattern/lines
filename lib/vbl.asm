2658fd01fe1ef3dec65ea48da00596bac44306d3
Thibaut Mattern <tmattern@adlere.fr>
2025-09-20 15:39:49 +0200

    correction WaitVBLAndSwitchBuffer

--- lib/vbl.asm
+++ lib/vbl.asm
@@ -14,6 +14,7 @@ InitScreen:
 ; === Routine d'attente VBL + switch buffer ===
 
 WaitVBLAndSwitchBuffer:
+    pshs  a
     ; -- Attendre que le bit 7 de $E7E5 passe à 1 (VBL active) --
 WaitVBL_Loop:
     lda   $E7E5
@@ -25,14 +26,29 @@ WaitVBL_End:
     bmi   WaitVBL_End    ; Tant que bit 7=1, on boucle (attend sortie VBL)
 
     ; -- Inverser la page --
-    ldx   #current_page
-    lda   ,x
+    lda   current_page
     eora  #1             ; inverse 0<->1
-    sta   ,x
+    sta   current_page
+    bne   WaitVBL_AffichePage1_MappePage0Cartouche
 
-    ; -- Mettre à jour le registre vidéo (page affichée) --
-    lda   ,x
-    asla                  ; bit 0 -> bit 1 (LSB to page select bit)
-    sta   $E7C0           ; $E7C0 = registre d'affichage (bit 1 = page)
+WaitVBL_AffichePage0_MappePage1Cartouche:
+    lda   $E7E4        ; Lire configuration actuelle (bordure/couleur)
+    anda  #$3F         ; Effacer D7-D6 (page affichée)
+    sta   $E7E4        ; Affiche page 0, bordure inchangée
 
-    rts
+    lda   #$61         ; %0110 0001 : D6=1 écriture autorisée, D5=1 RAM, D4-D0=1 (page 1)
+    sta   $E7E6        ; Mappe RAM page 1 dans espace cartouche
+
+    bra  WaitVBL_Exit
+
+WaitVBL_AffichePage1_MappePage0Cartouche:
+    lda   $E7E4        ; Lire configuration actuelle (bordure/couleur)
+    anda  #$3F         ; Effacer D7-D6 (page affichée)
+    ora   #$40         ; Mettre D7-D6 = 01 (page 1)
+    sta   $E7E4        ; Affiche page 1, bordure inchangée
+
+    lda   #$60         ; %0110 0000 : D6=1 écriture autorisée, D5=1 RAM, D4-D0=0 (page 0)
+    sta   $E7E6        ; Mappe RAM page 0 dans espace cartouche
+
+WaitVBL_Exit:
+    puls  a,pc
